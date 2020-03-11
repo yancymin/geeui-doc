f2c54d860670705291c99be020747b8375d6e5ce
yancymin <senseslofter@gmail.com>
2020-03-11 19:52:47 +0800

    Update routerData.js

--- src/components/routerData.js
+++ src/components/routerData.js
@@ -1,5 +1,5 @@
 const router = {
-    开始: ['关于 GEE-UI', '设计'],
+    开始: ['关于 Gee-UI', '设计'],
     基本: ['颜色', '文字', '间距', '布局', '插图'],
     组件: ['按钮 Buttons', '选择控件 Selection controls', '文本框 Text fields', '切换 Tabs', '列表 Lists', '卡片 Cards', '对话框 Dialogs', '药丸徽章 Pill badges', '提示 Tooltips', '时间选择器 Time picker', '日期选择器 Date picker', '地址选择器 Local selector', "步骤提示器 Step's tooltips", '数据表格 Data table', '面包屑导航 Breadcrumb', '上传 Upload', '全局提示 Message', '警报 Alert'],
     模式: ['表单 Forms', '空状态 Empty states', '全局头部 Global head', '侧边导航栏 Sidebar', '加载指示控件 Loading'],
@@ -7,7 +7,7 @@ const router = {
     帮助: '帮助'
 }
 const router2 = [
-    '/开始/关于 GEE-UI', '/开始/设计',
+    '/开始/关于 Gee-UI', '/开始/设计',
     '/基本/颜色', '/基本/文字', '/基本/间距', '/基本/布局', '/基本/插图',
     '/组件/按钮 Buttons', '/组件/选择控件 Selection controls', '/组件/文本框 Text fields', '/组件/切换 Tabs', '/组件/列表 Lists', '/组件/卡片 Cards', '/组件/对话框 Dialogs', '/组件/药丸徽章 Pill badges', '/组件/提示 Tooltips', '/组件/时间选择器 Time picker', '/组件/日期选择器 Date picker', '/组件/地址选择器 Local selector', "/组件/步骤提示器 Step's tooltips", '/组件/数据表格 Data table', '/组件/面包屑导航 Breadcrumb', '/组件/上传 Upload', '/组件/全局提示 Message', '/组件/警报 Alert',
     '/模式/表单 Forms', '/模式/空状态 Empty states', '/模式/全局头部 Global head', '/模式/侧边导航栏 Sidebar', '/模式/加载指示控件 Loading',
